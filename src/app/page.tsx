a1d3448551ab9819dd66bf271dc07c06bc40ae2b
Julliane Di Paula <dipaulajulliane@gmail.com>
2025-02-18 18:55:12 -0300

    Refatorando Usando o Biomejs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import { ArrowRight, Copy, Mail } from 'lucide-react'
 import { Button } from "@/components/button";
 import { IconButton } from '@/components/icon-button';
 import { InputField, InputIcon, InputRoot } from '@/components/input';
+import { ArrowRight, Copy, Mail } from 'lucide-react'
 
 export default function Home() {
   return (
